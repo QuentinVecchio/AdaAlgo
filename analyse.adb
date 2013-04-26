4679270d6208fb4ebbbd8b4bfbfe4ab4bec378a4
nicoptere <nicoptere@localhost>
2013-04-26 15:29:59 +0200

    debut d'ecriture des packages 'analyse' et 'tableau' plus la definition de types enum?r?s dans 'definition'

--- analyse.adb
+++ analyse.adb
@@ -1,27 +1,45 @@
+package body analyse is
+	
+	-------------------------
+	-- Procedure d'analyse --
+	-------------------------
+	procedure analyse(tab: in out T_tab_ligne; l_cour: natural; res: out T_Tab_Bloc) is
 
-procedure analyse(tab: in out T_tab_ligne; l_cour: natural; res: out T_Tab_Bloc) is
-	continue : boolean := True;
+	begin
+		while trouver une condition and tab'last /= L_cour loop
+		        type_ligne := GetType(Tab(l_cour));
+		        case type_ligne is
+		                when commentaire => Ajout_com(tab(l_cour), Res);
+		                when affectation => Ajout_aff(tab(l_cour), Res);
+		                when module      => Ajout_Mod(tab(l_cour), Res);
+		                when pour | tq   => Ajout_pour_tq (tab(l_cour), Res);
+		                when repeter     => Ajout_rep(tab, l_cour,Res);
+		                when cond        => Ajout_cond(tab, l_cour, Res);
+		                when others      => NULL;
+		        end case;
+		end loop;
+	end analyse;
 
-begin
-	while continue and Then tab'last /= L_cour loop
-		continue := False
-		type_ligne := GetType(Tab(l_cour));
-		case type_ligne is
-			when commentaire => Ajout_com(tab(l_cour), Res);
-					    continue := True;
-					    l_cour = l_cour + 1;
-			when affectation => Ajout_aff(tab(l_cour), Res);
-		    			    continue := True;
-					    l_cour = l_cour + 1;
-			when module 	 => Ajout_Mod(tab(l_cour), Res);
-					    continue := True;
-					    l_cour = l_cour + 1;
-			when pour | tq 	 => Ajout_pour_tq (tab(l_cour), Res);
-			when repeter	 => Ajout_rep(tab, l_cour,Res);
-			when cond	 => Ajout_cond(tab, l_cour, Res);
-			when others	 => NULL;
-		end case;
-	end loop;
+
+
+
+	-----------
+	-- AJOUT --
+	-----------
+	
+	--ajout d'un commentaire
+	procedure Ajout_com(L: chaine, Res : in out T_Tab_Bloc) is
+		blocCom : Bloc(commentaire);
+	begin
+		L := trimLeft(L);
+		L := trimRight(L);
+		L := substring(L, 2, length(L));
+		L := trimLeft(L);
+		blocCom.MonCom := L;
+		--utiliser une fonction pour la manipulation du type T_Tab_Bloc
+	
+	end Ajout_com;
+
+
+
 end analyse;
-
-
---
+++ analyse.ads
@@ -0,0 +1,20 @@
+--------------------------------------
+-- SPÉCIFICATION DU PACKAGE ANALYSE --
+--------------------------------------
+
+Package Analyse is
+
+	procedure analyse(tab: in out T_tab_ligne; l_cour: natural; res: out T_Tab_Bloc, );
+	-- analyse une ligne du code et envoie les information aux differentes procedures
+	
+	
+	
+	-- PROCEDURES D'AJOUT!! --
+	
+	procedure Ajout_com(L: chaine, Res : in out T_Tab_Bloc);
+
+
+
+
+
+end Package;
