d2af424038082769303be8e016dd2690fca3e216
nicoptere <nicoptere@localhost>
2013-04-26 16:45:24 +0200

    suppression d'une partie des bug, ...

--- analyse.ads
+++ analyse.ads
@@ -1,20 +1,25 @@
+with mstring; use mstring;
+with tableau; use tableau;
 --------------------------------------
 -- SPÉCIFICATION DU PACKAGE ANALYSE --
 --------------------------------------
 
-Package Analyse is
+Package analyse is
+	type T_Tab_Ligne is array (integer range 1..100) of chaine;
 
-	procedure analyse(tab: in out T_tab_ligne; l_cour: natural; res: out T_Tab_Bloc, );
+	procedure analyse(tab: in out T_tab_ligne; l_cour : natural; res: out T_Tab_Bloc);
 	-- analyse une ligne du code et envoie les information aux differentes procedures
 	
 	
 	
-	-- PROCEDURES D'AJOUT!! --
 	
-	procedure Ajout_com(L: chaine, Res : in out T_Tab_Bloc);
+	------------------------
+	-- PROCEDURES D'AJOUT --
+	------------------------	
+	procedure Ajout_com(L: chaine; Res : in out T_Tab_Bloc);
 
 
 
 
 
-end Package;
+end analyse;
