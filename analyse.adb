316429cc4f006321c609121edb0d247869017b0e
nicoptere <11332726+nicoptere2@users.noreply.github.com>
2013-04-26 12:01:43 +0200

    ajout de la boucle dans la procedure analyse

--- analyse.adb
+++ analyse.adb
@@ -1,13 +1,21 @@
 
 procedure analyse(tab: in out T_tab_ligne; l_cour: natural; res: out T_Tab_Bloc) is
+	continue : boolean := True;
 
 begin
-	while trouver une condition and tab'last /= L_cour loop
+	while continue and Then tab'last /= L_cour loop
+		continue := False
 		type_ligne := GetType(Tab(l_cour));
 		case type_ligne is
 			when commentaire => Ajout_com(tab(l_cour), Res);
+					    continue := True;
+					    l_cour = l_cour + 1;
 			when affectation => Ajout_aff(tab(l_cour), Res);
+		    			    continue := True;
+					    l_cour = l_cour + 1;
 			when module 	 => Ajout_Mod(tab(l_cour), Res);
+					    continue := True;
+					    l_cour = l_cour + 1;
 			when pour | tq 	 => Ajout_pour_tq (tab(l_cour), Res);
 			when repeter	 => Ajout_rep(tab, l_cour,Res);
 			when cond	 => Ajout_cond(tab, l_cour, Res);
