fe4987d46a27d8595764760518df2cc3f7339bbf
Matthieu Clin <matthieu.clin@outlook.com>
2013-05-05 11:13:48 +0200

    ajout travail de nicolas, corrections pour compilation

---
+++ app/analyse.ads
@@ -0,0 +1,26 @@
+with mstring, gestionbloc, definitions;
+use mstring, gestionbloc, definitions;
+--------------------------------------
+-- SPÉCIFICATION DU PACKAGE ANALYSE --
+--------------------------------------
+
+Package analyse is
+	type T_Tab_Ligne is array (integer range 1..100) of chaine;
+
+	procedure analyse(tab: in out T_tab_ligne; l_cour : natural; res: out T_Tab_Bloc);
+	-- analyse une ligne du code et envoie les information aux differentes procedures
+	
+	
+	
+	function GetType(ligne: chaine)return T_type_ligne;
+	
+	------------------------
+	-- PROCEDURES D'AJOUT --
+	------------------------	
+	procedure Ajout_com(L: chaine; Res : in out T_Tab_Bloc);
+
+
+
+
+
+end analyse;
