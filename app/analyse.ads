02ec59952ca3ee72824628c9dc2699c1650c3a37
Matthieu Clin <matthieu.clin@outlook.com>
2013-05-05 11:37:12 +0200

    ajout procedure vide d'ajout, pour d?commenter procedure analyse

--- app/analyse.ads
+++ app/analyse.ads
@@ -7,9 +7,8 @@ use mstring, gestionbloc, definitions;
 Package analyse is
 	type T_Tab_Ligne is array (integer range 1..100) of chaine;
 
-	procedure analyse(tab: in out T_tab_ligne; l_cour : natural; res: out T_Tab_Bloc);
+	procedure analyse(tab: in out T_tab_ligne; l_cour : in out natural; res: out T_Tab_Bloc);
 	-- analyse une ligne du code et envoie les information aux differentes procedures
-	
 	
 	
 	function GetType(ligne: chaine)return T_type_ligne;
@@ -19,8 +18,13 @@ Package analyse is
 	------------------------	
 	procedure Ajout_com(L: chaine; Res : in out T_Tab_Bloc);
 
+	procedure Ajout_aff(L: chaine; res: T_Tab_Bloc);
 
+	procedure Ajout_Mod(L: chaine; res: T_Tab_Bloc);
+	
+	procedure Ajout_pour_tq (tab: T_tab_ligne ; l_cour: in out natural; Res: T_Tab_Bloc);
 
-
-
+	procedure Ajout_rep (tab: T_tab_ligne ; l_cour: in out natural; Res: T_Tab_Bloc);
+	
+	procedure Ajout_cond (tab: T_tab_ligne ; l_cour: in out natural; Res: T_Tab_Bloc);
 end analyse;
