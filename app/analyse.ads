34f702cb43d309feb5a4a953ecc4328f0aae318d
Matthieu Clin <matthieu.clin@outlook.com>
2013-05-05 12:17:50 +0200

    mise en commun de T_Tab_ligne dans definitions

--- app/analyse.ads
+++ app/analyse.ads
@@ -5,7 +5,6 @@ use mstring, gestionbloc, definitions;
 --------------------------------------
 
 Package analyse is
-	type T_Tab_Ligne is array (integer range 1..100) of chaine;
 
 	procedure analyse(tab: in out T_tab_ligne; l_cour : in out natural; res: out T_Tab_Bloc);
 	-- analyse une ligne du code et envoie les information aux differentes procedures
