f9b7f557545f7141a42f560a2613c84b4fd9b631
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2013-05-18 11:43:44 +0200

    ajout fonction de construction du type ligne, un par type possible

--- app/entetelexique.adb
+++ app/entetelexique.adb
@@ -8,4 +8,45 @@ package body entetelexique is
 			return l;
 	end donnerFonction;
 
+	function donnerModule(nom, commentaire: chaine) return ligne is
+		l: ligne(module);
+		
+		begin
+		
+			return l;
+	end donnerModule;
+	
+	function donnerVariable(nom, commentaire, typeValeur: chaine) return ligne is
+		l: ligne(variable);
+		
+		begin
+		
+			return l;
+	end donnerVariable;
+	
+	
+	function donnerConstante(nom, commentaire, typeValeur, valeur: chaine) return ligne is
+		l: ligne(constante);
+		
+		begin
+		
+			return l;
+	end donnerConstante;
+	
+	function donnerTable(nom, commentaire, intervalle, typeElement: chaine) return ligne is
+		l: ligne(table);
+		
+		begin
+		
+			return l;
+	end donnerTable;
+	
+	function donnerStructure(nom, commentaire, ensElement: chaine) return ligne is
+		l: ligne(structure);
+		
+		begin
+		
+			return l;
+	end donnerStructure;
+	
 end entetelexique;
