9ba56542787e9f8f538a0e485ccf4240040944d5
Matthieu Clin <matthieu.clin@outlook.com>
2013-05-18 11:32:54 +0200

    ajout partie de cindy

---
+++ app/entetelexique.adb
@@ -0,0 +1,11 @@
+package body entetelexique is
+
+	function donnerFonction(nom, commentaire, typeRetour: chaine) return ligne is
+		l: ligne(fonction);
+		
+		begin
+		
+			return l;
+	end donnerFonction;
+
+end entetelexique;
