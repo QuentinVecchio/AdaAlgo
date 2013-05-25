5ca6aa541851f59f7b6d64894426aa6fb71286dc
nicoptere <nicoptere@localhost>
2013-05-25 11:18:47 +0200

    mise en place de commentaire digne de ce nom dans 'analyse.ads'

--- app/analyse.ads
+++ app/analyse.ads
@@ -21,30 +21,50 @@ Package analyse is
 	
 	--
 	--	Permet d'ajouter un commentaire
+	--	@param L, ligne contenant le commentaire
+	--	@param res, liste de bloc
 	--
 	procedure Ajout_com(L: chaine; Res : in out T_Tab_Bloc);
 
 	--
 	--	Permet d'ajouter une ligne correspondant a une affectation
+	--	@param L, ligne contenant l'affectation
+	--	@param res, liste de bloc
 	--
 	procedure Ajout_aff(L: chaine; res: T_Tab_Bloc);
 
 	--
 	--	Permet d'ajouter une ligne correspondant à un appel de module
+	--	@param L, ligne contenant le module
+	--	@pram res, liste de bloc
 	--
 	procedure Ajout_Mod(L: chaine; res: T_Tab_Bloc);
 	
 	--
-	--	Permet d'ajouter un bloc 'pour', 'tq' et 'repeter' 
-	--	C'est à dire de 'pour'-> 'fpour', 'tq' -> 'ftq' et 'repeter'->'jq'
-	--	la procedure dispatche ensuite vers les fonction d'ajout qui corresponde au type de la boucle
+	--	met en forme la condition pour etre implementé en mémoire
+	--	appel la procedure 'Analyse_Code' sur l'interrieur de la boucle
+	--	ajoute la condition, et l'anayle du bloc dans la liste de bloc
+	--	@param tab, le tableau de ligne
+	--	@param Res, liste de bloc 
 	--
-
-	
 	procedure Ajout_Pour(tab: in out T_tab_ligne; Res: in out T_Tab_Bloc);
 
+	--
+	--	met en forme la condition pour etre implementé en mémoire
+	--	appel la procedure 'Analyse_Code' sur l'interrieur de la boucle
+	--	ajoute la condition, et l'anayle du bloc dans la liste de bloc
+	--	@param tab, le tableau de ligne
+	--	@param Res, liste de bloc 
+	--
 	procedure Ajout_tq(tab: in out T_tab_ligne; Res: in out T_Tab_Bloc);
 	
+	--
+	--	met en forme la condition pour etre implementé en mémoire
+	--	appel la procedure 'Analyse_Code' sur l'interrieur de la boucle
+	--	ajoute la condition, et l'anayle du bloc dans la liste de bloc
+	--	@param tab, le tableau de ligne
+	--	@param Res, liste de bloc 
+	--
 	procedure Ajout_Repeter(tab: in out T_tab_ligne; Res: in out T_Tab_Bloc);
 	
 	
@@ -54,6 +74,8 @@ Package analyse is
 	--	C'est à dire de si -> fsi
 	--	Modifie l'indice de la ligne courant, pointe sur  fsi
 	--	Renvoit l'ensemble des blocs contenus dans le bloc courant
+	--	@param tab, liste de ligne a partire du début de la condition
+	--	@param res, liste de bloc
 	--
 	procedure Ajout_cond (tab: T_tab_ligne ;  Res: T_Tab_Bloc);
 	
