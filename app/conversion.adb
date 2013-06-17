eaca025de0ae3055d712447bf6fbeec0d322cd18
Matthieu Clin <matthieu.clin@outlook.com>
2013-06-17 16:37:16 +0200

    correction bug de nicolas

--- app/conversion.adb
+++ app/conversion.adb
@@ -33,10 +33,10 @@ package body conversion is
 	
 	procedure conversionAffectation(m_bloc : in out Bloc; Ligne : in out T_TAB_LIGNE) is
 	begin
-		if contains(m_bloc.vD, " mod ") AND THEN NOT(StartWith(m_bloc.vD, " mod ")then
-			replaceStr(m_bloc.vD, "mod", "rem", m_bloc.vD);
-		elsif contains(m_bloc.vD, " div ") AND THEN NOT(StartWith(m_bloc.vD, " div ")
-			replaceStr(m_bloc.vD, "div", "/", m_bloc.vD);
+		if contains(m_bloc.vD, " mod ") AND THEN NOT(StartWith(m_bloc.vD, " mod "))then
+			 m_bloc.vD:=replaceStr(m_bloc.vD, "mod", "rem");
+		elsif contains(m_bloc.vD, " div ") AND THEN NOT(StartWith(m_bloc.vD, " div "))then
+			 m_bloc.vD:=replaceStr(m_bloc.vD, "div", "/");
 		end if;
 		Ajout_queue(Ligne,m_bloc.vG + ":=" + m_bloc.vD + ";");
 	end conversionAffectation;
