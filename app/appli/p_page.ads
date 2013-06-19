accfc5700e0e95a9602748a53bbde67442697e45
nicoptere <11332726+nicoptere2@users.noreply.github.com>
2013-06-19 15:19:00 +0200

    merge

--- app/appli/p_page.ads
+++ app/appli/p_page.ads
@@ -10,35 +10,44 @@ WITH Gtk.Notebook;	USE Gtk.Notebook;
 WITH Gtk.Scrolled_Window;	USE Gtk.Scrolled_Window;
 WITH Gtk.Adjustment;	USE Gtk.Adjustment;
 WITH Glib;		USE Glib;
+WITH Gtk.Separator;	USE Gtk.Separator;
 WITH Ada.Finalization; 	USE Ada.Finalization;
 
 PACKAGE P_Page IS
 type tab_code is array(1..5) of Gtk_Text_View;
+type tab_ada is array(1..5) of Gtk_Text_View;
 type tab_var is array(1..5) of Gtk_Text_View;
 type tab_label is array(1..5) of Gtk_Label;
 type tab_btn is array(1..5) of Gtk_Button;
 type tab_Table is array(1..5) of Gtk_Table;
-
+type tab_Sepatateur is array(1..5) of Gtk_Vseparator;
+	
 	TYPE T_Page IS NEW Controlled WITH RECORD
-		--Zones de saisie
-			
-			zoneCode : tab_code;
-			
+		--Zones de saisie			
+			zoneCode : tab_code;			
 			zoneVariable : tab_var;
 		--label			
 			labelTitre : tab_label;
+			labelCode, labelVariable, labelAda : tab_label;
 		--button
 			btnFermer : tab_btn;
+		--Zone ada
+			zoneAda : tab_ada;
 		--Barre de défilement
-			barre : Gtk_Scrolled_Window;
+			barre1,barre2,barre3 : Gtk_Scrolled_Window;
 		--Ajustement
 			ajust : Gtk_Adjustment;		
 		--table & Boite	
 			Table : tab_Table;
+		--couleur
+			couleur : Gdk_Color;
+			couleur2 : Gdk_Color;
 		--image
 			imageFermer : Gtk_Image;
 		--NoteBook
 			onglet : Gtk_Notebook;
+		--separateur
+			separateur : tab_Sepatateur; 
 	END RECORD;
 
 	PROCEDURE Initialize(P : IN OUT T_Page);
