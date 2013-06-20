56cc8615236f95938d4086b4ee3b67b1e160877f
nicoptere <11332726+nicoptere2@users.noreply.github.com>
2013-06-20 10:02:38 +0200

    ajout d'une fonction 'SplitChaine'

--- app/debug.adb
+++ app/debug.adb
@@ -1,33 +1,36 @@
 with definitions; use definitions;
 package body debug is
 
-	procedure debuggage(tab: in out T_tab_ligne) is
+	procedure debuggage(tab: in out T_tab_ligne; descrErrors : in out T_Tab_Ligne) is
 		type_ligne: T_type_ligne;
 		tmp : T_tab_ligne := tab;
 		erreur : boolean := false;
 		nbLigne : integer := 0;
+		--descrErrors : T_Tab_Ligne;
+		coucou : chaine;
 	begin
  		while NOT estVide(tmp) loop
 			if length(trimLeft(trimRight(donne_tete(tmp)))) > 2 then
+				erreur := False;
 				nbLigne := nbLigne + 1;
 		                type_ligne := GetType(donne_tete(tmp));
 				if type_ligne = inconnu then
-					put_line("le type de ligne n'a pas ete reconnu, a la ligne : "+integer'image(nbLigne)+" ,copie de la ligne "+ donne_tete(tmp));
+					put_line("le type de ligne n'a pas ete reconnu, a la ligne : "+integer'image(nbLigne)+" ,copie de la ligne  => "+ donne_tete(tmp));
 					erreur := true;
 				else
 				        case type_ligne is
 				                when commentaire => NULL;
-				                when affectation => erreur := debug_aff(donne_tete(tmp));
-				                when module      => erreur := debug_Mod(donne_tete(tmp));
-				                when pour        => erreur := debug_Pour (donne_tete(tmp));
-				                when tq          => erreur := debug_tq (donne_tete(tmp));
-				                when repeter     => erreur := debug_repeter (donne_tete(tmp));
-				                when cond        => erreur := debug_cond(donne_tete(tmp));
-				                when testcase    => erreur := debug_case(donne_tete(tmp));
+				                when affectation => erreur := debug_aff(donne_tete(tmp), descrErrors);
+				                when module      => erreur := debug_Mod(donne_tete(tmp), descrErrors);
+				                when pour        => erreur := debug_Pour (donne_tete(tmp), descrErrors);
+				                when tq          => erreur := debug_tq (donne_tete(tmp), descrErrors);
+				                when repeter     => erreur := debug_repeter (donne_tete(tmp), descrErrors);
+				                when cond        => erreur := debug_cond(donne_tete(tmp), descrErrors);
+				                when testcase    => erreur := debug_case(donne_tete(tmp), descrErrors);
 				                when others      => EXIT;  -- sinon, sinonsi ,fsi, fpour, ftq, jusqu'a
 				        end case;
 					if erreur then
-						put_line("il y a une erreur a la ligne : "+integer'image(nbLigne)+" copie de la ligne "+ donne_tete(tmp));
+						put_line("il y a une erreur a la ligne : "+integer'image(nbLigne)+" copie de la ligne => "+ donne_tete(tmp));
 					end if;
 				end if;
 			end if;
@@ -81,12 +84,12 @@ package body debug is
 
 
 
-function debug_aff(L : in chaine) return boolean is
+function debug_aff(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
 begin
 	return False;
 end debug_aff;
 
-function debug_Mod(L : in chaine) return boolean is
+function debug_Mod(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
 	tmp : chaine := L;
 	erreur : boolean := False;
 	guillemet : string(1..1);
@@ -103,7 +106,7 @@ begin
 	i := strpos(tmp, guillemet(1));
 	if i /= 0 Then
 		if strpos(substring(tmp, i+1, length(tmp)), guillemet(1)) = 0 then
-			put_line(CreateChaine("les guillemets n'ont pas été fermés"));
+			Ajout_queue(descr, CreateChaine("les guillemets n'ont pas été fermés"));
 			erreur := True;
 		end if;
 	end if;
@@ -112,39 +115,43 @@ begin
 	i := strpos(tmp, '(');
 	if i = 0 then
 		erreur := erreur AND True;
-		put_line(CreateChaine("il n'y a pas de parenthese ouvrante"));
+		Ajout_queue(descr, CreateChaine("il n'y a pas de parenthese ouvrante"));
 
 	end if;
 	if strpos(tmp, ')') = 0 then
-		put_line(CreateChaine("il n'y a pas de parenthese fermente"));
+		Ajout_queue(descr, CreateChaine("il n'y a pas de parenthese fermente"));
 		erreur := erreur AND True;
 	end if;
 	if i > strpos(tmp, ')') then
-		put_line(CreateChaine("Vous avez mal géré vos parenthese"));
+		Ajout_queue(descr, CreateChaine("Vous avez mal géré vos parenthese"));
 		erreur := erreur AND True;
 	end if;
 
 
-	--verification qu'il y ait au moins un parametre
-	tmp := substring(tmp, strpos(tmp, '('), strpos(tmp, ')'));
-	tmp := trimLeft(tmp);
-	tmp := trimRight(tmp);
-	if length(tmp) =2 then
-		erreur := erreur AND True;
-		put_line(CreateChaine("il faut mettre au moins un parametre dans cette fonction"));
+	if NOT erreur then
+		--verification qu'il y ait au moins un parametre
+		tmp := substring(tmp, strpos(tmp, '('), strpos(tmp, ')'));
+		tmp := trimLeft(tmp);
+		tmp := trimRight(tmp);
+		if length(tmp) =2 then
+			erreur := erreur AND True;
+			Ajout_queue(descr, CreateChaine("il faut mettre au moins un parametre dans cette fonction"));
+		end if;
 	end if;
 	return erreur;
 
 
 end debug_Mod;
 
-function debug_Pour(L : in chaine) return boolean is
+function debug_Pour(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
 	bi, bs : chaine;
 	binf, bsup : string(1..10); m,n : integer;
 	tmp: chaine := L; 
 	inf, sup : integer;
 	erreur : boolean := False;
+	ss_chaine : T_Tab_Ligne;
 begin
+	-- Verification des bornes inf et des bornes sup!
 	bi := substring(tmp, strpos(tmp, '-')+1, strpos(tmp, 'a')-1);
         bs := substring(tmp, strpos(tmp, 'a')+1, length(tmp)-6)+" ";
 	bi := trimLeft(bi); bs := trimLeft(bs);
@@ -155,33 +162,41 @@ begin
 	toString(bi, binf, m);
 	tostring(bs, bsup, n);	
 	
-	--ERREUR ICI : raised CONSTRAINT_ERROR : s-valuti.adb:273 explicit raise
-	--inf := integer'Value(binf);
-	--sup := integer'Value(bsup);
+	inf := integer'Value(binf(1..m));
+	sup := integer'Value(bsup(1..n));
 
 	if inf >= sup then
-		put_line(CreateChaine("euh... vous avez mal gérer vous bornes inf et sup"));
+		Ajout_queue(descr, CreateChaine("euh... vous avez mal gérer vous bornes inf et sup"));
 		erreur := True;
 	end if;
+
+	-- Verification de la présence de '<-' 'a' et 'faire'
+	tmp := L;
+	SplitChaine(tmp, ss_chaine);
+	if appartient_liste(tmp, CreateChaine("<-")) AND appartient_liste(tmp, CreateChaine("a")) AND appartient_liste(tmp, CreateChaine("faire")) then
+		Ajout_queue(descr, CreateChaine("il manque des elements dans votre condition"));
+		erreur := erreurs AND False;
+	end if
+	
 	return erreur;
 end debug_Pour;
 
-function debug_tq(L : in chaine) return boolean is
+function debug_tq(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
 begin
 	return False;
 end debug_tq;
 
-function debug_repeter(L : in chaine) return boolean is
+function debug_repeter(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
 begin
 	return False;
 end debug_repeter;
 
-function debug_cond(L : in chaine) return boolean is
+function debug_cond(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
 begin
 	return False;
 end debug_cond;
 
-function debug_case(L : in chaine) return boolean is
+function debug_case(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
 begin
 	return False;
 end debug_case;
@@ -211,5 +226,33 @@ begin
 	end loop;
 	return ok;
 end estUneVariable;
+
+
+procedure afficheErrors(descr : T_Tab_ligne) is
+
+begin
+	if NOT estVide(descr) then
+		put_line(donne_Tete(descr));
+		afficheErrors(donne_suivant(descr));
+	end if;
+
+end afficheErrors;
+
+procedure SplitChaine(c : in out Chaine; chaineSplit : in out T_Tab_Ligne) is
+	i : integer;
+begin
+	c := trimLeft(c); c:= trimRight(c);
+	c := c+' ';
+	if length(c) > 0 then
+		i := strpos(c, ' ');
+		Ajout_queue(chaineSplit, substring(c, 1, i -1));
+		if i < length(c) then
+			c := substring(c, i+1, length(c));
+			SplitChaine(c, chaineSplit);
+		end if;
+	end if;
+end SplitChaine;
+
+
 
 end debug;
