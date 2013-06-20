06ece9745b90c1713988b7b3f999ef17bb778a65
nicoptere <nicoptere@localhost>
2013-06-20 16:54:06 +0200

    modificationdes descriptions d'erreurs

--- app/debug.adb
+++ app/debug.adb
@@ -8,38 +8,41 @@ package body debug is
 		nbLigne : integer := 0;
 
 	begin
-		ok2 := analyseInclusionBloc(tab, descrErrors);
+		
+		ok := analyseInclusionBloc(tab, descrErrors);
+		if ok then
+	 		while NOT estVide(tmp) loop
+				if length(trimLeft(trimRight(donne_tete(tmp)))) > 2 then
 
- 		while NOT estVide(tmp) loop
-			if length(trimLeft(trimRight(donne_tete(tmp)))) > 2 then
-
-				ok2 := True;
-				nbLigne := nbLigne + 1;
-		                type_ligne := GetType(donne_tete(tmp));
+					ok2 := True;
+					nbLigne := nbLigne + 1;
+				        type_ligne := GetType(donne_tete(tmp));
 
 					
 
-				if type_ligne = inconnu then
-					put_line("le type de ligne n'a pas ete reconnu, a la ligne : "+integer'image(nbLigne)+" ,copie de la ligne  => "+ donne_tete(tmp));
-					ok2 := False;
-				else
-					case type_ligne is
-					        when commentaire => NULL;
-					        when affectation => ok2 := debug_aff(donne_tete(tmp), descrErrors);
-					        when module      => ok2 := debug_Mod(donne_tete(tmp), descrErrors);
-					        when pour        => ok2 := debug_Pour (donne_tete(tmp), descrErrors);
-					        when tq          => ok2 := debug_tq (donne_tete(tmp), descrErrors);
-					        when repeter     => ok2 := debug_repeter (donne_tete(tmp), descrErrors);
-					        when cond        => ok2 := debug_cond(donne_tete(tmp), descrErrors);
-					        when testcase    => ok2 := debug_case(donne_tete(tmp), descrErrors);
-					        when others      => EXIT;  -- sinon, sinonsi ,fsi, fpour, ftq, jusqu'a
-					end case;
+					if type_ligne = inconnu then
+						Ajout_queue(descrErrors, "Le type de ligne n'a pas ete reconnu : "+ donne_tete(tmp));
+						ok2 := False;
+					else
+					
+						case type_ligne is
+							when commentaire => NULL;
+							when affectation => ok2 := debug_aff(donne_tete(tmp), descrErrors);
+							when module      => ok2 := debug_Mod(donne_tete(tmp), descrErrors);
+							when pour        => ok2 := debug_Pour (donne_tete(tmp), descrErrors);
+							when tq          => ok2 := debug_tq (donne_tete(tmp), descrErrors);
+							when repeter     => ok2 := debug_repeter (donne_tete(tmp), descrErrors);
+							when cond        => ok2 := debug_cond(donne_tete(tmp), descrErrors);
+							when testcase    => ok2 := debug_case(donne_tete(tmp), descrErrors);
+							when others      => NULL;  -- sinon, sinonsi ,fsi, fpour, ftq, jusqu'a
+						end case;
+					end if;
 				end if;
-			end if;
 
-                        donne_suivant(tmp);
-			ok := ok AND ok2;
-                end loop;
+		                donne_suivant(tmp);
+				ok := ok AND ok2;
+		        end loop;
+		end if;
 
 		return ok;
 	end debuggage;
@@ -103,7 +106,6 @@ function debug_Mod(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
 	guillemet : string(1..1);
 	i : integer;
 begin
-	put_line(CreateChaine("Verification de module"));
 	guillemet(1) := character'val(34);
 	-- ouvire quote mais pas fermé
 	-- presence de parentheses
@@ -114,7 +116,7 @@ begin
 	i := strpos(tmp, guillemet(1));
 	if i /= 0 Then
 		if strpos(substring(tmp, i+1, length(tmp)), guillemet(1)) = 0 then
-			Ajout_queue(descr, CreateChaine("les guillemets n'ont pas été fermés"));
+			Ajout_queue(descr, "Erreur de pairage des guillemets : " + L);
 			ok := ok AND False;
 		end if;
 	end if;
@@ -123,15 +125,15 @@ begin
 	i := strpos(tmp, '(');
 	if i = 0 then
 		ok := ok AND False;
-		Ajout_queue(descr, CreateChaine("il n'y a pas de parenthese ouvrante"));
+		Ajout_queue(descr, "Parenthese ouvrante '(' : "+L);
 
 	end if;
 	if strpos(tmp, ')') = 0 then
-		Ajout_queue(descr, CreateChaine("il n'y a pas de parenthese fermente"));
+		Ajout_queue(descr, "Parenthese fermante ')' : "+L);
 		ok := ok AND False;
 	end if;
 	if i > strpos(tmp, ')') AND strpos(tmp, '(') /= 0 then
-		Ajout_queue(descr, CreateChaine("Vous avez mal géré vos parenthese"));
+		Ajout_queue(descr, "Erreur de parenthesage : " + L);
 		ok := ok AND False;
 	end if;
 
@@ -143,7 +145,7 @@ begin
 		tmp := trimRight(tmp);
 		if length(tmp) =2 then
 			ok := ok AND False;
-			Ajout_queue(descr, CreateChaine("il faut mettre au moins un parametre dans cette fonction"));
+			Ajout_queue(descr, "Parametre manquant : "+L);
 		end if;
 	end if;
 
@@ -163,14 +165,12 @@ begin
 	-- Verification de la présence de '<-' 'a' et 'faire'
 	SplitChaine(tmp, ss_chaine);
 	if (appartient_liste(ss_chaine, CreateChaine("<-")) AND appartient_liste(ss_chaine, CreateChaine("a")) AND appartient_liste(ss_chaine, CreateChaine("faire"))) then
-		Ajout_queue(descr, CreateChaine("il manque des elements dans votre condition"));
+		Ajout_queue(descr, "Condition manquante : " + L);
 		ok := ok AND False;
 	else
 		-- Verification des bornes inf et des bornes sup!
 
 		bi := substring(L, strpos(L, "<-")+2, strpos(L, 'a')-1);
-		put_line(CreateChaine("la condition du pour"));
-		put_line(bi);
 		bs := substring(L, strpos(L, 'a')+1, length(L)-6);
 		bi := trimLeft(bi); bs := trimLeft(bs);
 		bi := trimRight(bi); bs := trimRight(bs);
@@ -182,7 +182,7 @@ begin
 		sup := integer'Value(bsup(1..n));
 
 		if inf >= sup then
-			Ajout_queue(descr, CreateChaine("euh... vous avez mal gérer vous bornes inf et sup"));
+			Ajout_queue(descr, "Intervalle mal defini : "+ L);
 			ok := False AND ok;
 		end if;
 
@@ -206,12 +206,12 @@ begin
 	SplitChaine(tmp, ss_chaine);
 	enleve_enTete(ss_chaine);
 	if NOT(donne_queue(ss_chaine) = CreateChaine("faire")) then
-		Ajout_queue(descr, CreateChaine("Il manque le faire a la fin de la ligne"));
+		Ajout_queue(descr, "Mot-clef 'faire' manquant : "+L);
 		ok := False AND ok;
 	else
 		enleve_queue(ss_chaine);
 		if estVide(ss_chaine) then 
-			Ajout_queue(descr, CreateChaine("il n'y a pas de condition"));
+			Ajout_queue(descr, "Condition manquante : "+L);
 			ok := False AND ok;
 		end if;
 	end if;
@@ -240,13 +240,11 @@ begin
 		if Type_Ligne in cond | lignecas | pour | tq | repeter then
 			Ajout_queue(pile_elt, type_ligne);
 		end if;
-		put_line(CreateChaine("Avant suppression: "));
-		Affiche_liste(pile_elt);
+
 		if Type_Ligne in fsi | jqa | fpour | ftq then
 			enleve_queue(pile_elt);
 		end if;
-		put_line(CreateChaine("apres suppression: "));
-		Affiche_liste(pile_elt);
+
 		donne_suivant(tmp);
 	end loop;
 	if NOT estVide(pile_elt) then
