14b080637acad7f0c2021c7bf9d4c415b010aded
nicoptere <11332726+nicoptere2@users.noreply.github.com>
2013-06-20 16:17:38 +0200

    les fonctions ecrites marchent bien, il faut en ajouter

--- app/debug.adb
+++ app/debug.adb
@@ -1,37 +1,46 @@
 with definitions; use definitions;
 package body debug is
 
-	function debuggage(tab: in out T_tab_ligne; descrErrors : in out T_Tab_Ligne) return boolean is
+	function debuggage(tab: in T_tab_ligne; descrErrors : in out T_Tab_Ligne) return boolean is
 		type_ligne: T_type_ligne;
 		tmp : T_tab_ligne := tab;
-		ok : boolean := True;
+		ok, ok2 : boolean := True;
 		nbLigne : integer := 0;
 
 	begin
+		ok2 := analyseInclusionBloc(tab, descrErrors);
+
  		while NOT estVide(tmp) loop
 			if length(trimLeft(trimRight(donne_tete(tmp)))) > 2 then
-				ok := False;
+
+				ok2 := True;
 				nbLigne := nbLigne + 1;
 		                type_ligne := GetType(donne_tete(tmp));
+
+					
+
 				if type_ligne = inconnu then
 					put_line("le type de ligne n'a pas ete reconnu, a la ligne : "+integer'image(nbLigne)+" ,copie de la ligne  => "+ donne_tete(tmp));
-					ok := False;
+					ok2 := False;
 				else
-				        case type_ligne is
-				                when commentaire => NULL;
-				                when affectation => ok := debug_aff(donne_tete(tmp), descrErrors);
-				                when module      => ok := debug_Mod(donne_tete(tmp), descrErrors);
-				                when pour        => ok := debug_Pour (donne_tete(tmp), descrErrors);
-				                when tq          => ok := debug_tq (donne_tete(tmp), descrErrors);
-				                when repeter     => ok := debug_repeter (donne_tete(tmp), descrErrors);
-				                when cond        => ok := debug_cond(donne_tete(tmp), descrErrors);
-				                when testcase    => ok := debug_case(donne_tete(tmp), descrErrors);
-				                when others      => EXIT;  -- sinon, sinonsi ,fsi, fpour, ftq, jusqu'a
-				        end case;
+					case type_ligne is
+					        when commentaire => NULL;
+					        when affectation => ok2 := debug_aff(donne_tete(tmp), descrErrors);
+					        when module      => ok2 := debug_Mod(donne_tete(tmp), descrErrors);
+					        when pour        => ok2 := debug_Pour (donne_tete(tmp), descrErrors);
+					        when tq          => ok2 := debug_tq (donne_tete(tmp), descrErrors);
+					        when repeter     => ok2 := debug_repeter (donne_tete(tmp), descrErrors);
+					        when cond        => ok2 := debug_cond(donne_tete(tmp), descrErrors);
+					        when testcase    => ok2 := debug_case(donne_tete(tmp), descrErrors);
+					        when others      => EXIT;  -- sinon, sinonsi ,fsi, fpour, ftq, jusqu'a
+					end case;
 				end if;
 			end if;
+
                         donne_suivant(tmp);
+			ok := ok AND ok2;
                 end loop;
+
 		return ok;
 	end debuggage;
 
@@ -59,7 +68,6 @@ package body debug is
                         typeligne:= fsi;
                 elsif(startWith(ligne, "lire") or else startWith(ligne, "ecrire"))then
                         typeligne:= module;
-
                 elsif(startWith(ligne, "fpour"))then
                         typeligne:= fpour;
                 elsif(startWith(ligne, "ftq"))then
@@ -72,6 +80,8 @@ package body debug is
                         typeligne:= jqa;
                 elsif(startWith(ligne, "cas"))then
                         typeligne:= testcase;
+                elsif(startWith(ligne, "fcas"))then
+                        typeligne:= fcas;
 		else
 			typeligne:= inconnu;
                 end if;
@@ -82,13 +92,14 @@ package body debug is
 
 
 function debug_aff(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
+	ok : boolean := True;
 begin
-	return False;
+	return ok;
 end debug_aff;
 
 function debug_Mod(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
 	tmp : chaine := L;
-	ok : boolean := False;
+	ok : boolean := True;
 	guillemet : string(1..1);
 	i : integer;
 begin
@@ -104,7 +115,7 @@ begin
 	if i /= 0 Then
 		if strpos(substring(tmp, i+1, length(tmp)), guillemet(1)) = 0 then
 			Ajout_queue(descr, CreateChaine("les guillemets n'ont pas été fermés"));
-			ok := False;
+			ok := ok AND False;
 		end if;
 	end if;
 
@@ -125,7 +136,7 @@ begin
 	end if;
 
 
-	if NOT ok then
+	if ok then
 		--verification qu'il y ait au moins un parametre
 		tmp := substring(tmp, strpos(tmp, '('), strpos(tmp, ')'));
 		tmp := trimLeft(tmp);
@@ -135,6 +146,7 @@ begin
 			Ajout_queue(descr, CreateChaine("il faut mettre au moins un parametre dans cette fonction"));
 		end if;
 	end if;
+
 	return ok;
 
 
@@ -149,19 +161,19 @@ function debug_Pour(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
 	ss_chaine : T_Tab_Ligne;
 begin
 	-- Verification de la présence de '<-' 'a' et 'faire'
-	tmp := L;
 	SplitChaine(tmp, ss_chaine);
-	if NOT (appartient_liste(ss_chaine, CreateChaine("<-")) AND appartient_liste(ss_chaine, CreateChaine("a")) AND appartient_liste(ss_chaine, CreateChaine("faire"))) then
+	if (appartient_liste(ss_chaine, CreateChaine("<-")) AND appartient_liste(ss_chaine, CreateChaine("a")) AND appartient_liste(ss_chaine, CreateChaine("faire"))) then
 		Ajout_queue(descr, CreateChaine("il manque des elements dans votre condition"));
 		ok := ok AND False;
 	else
 		-- Verification des bornes inf et des bornes sup!
-		bi := substring(tmp, strpos(tmp, '-')+1, strpos(tmp, 'a')-1);
-		bs := substring(tmp, strpos(tmp, 'a')+1, length(tmp)-6)+" ";
+
+		bi := substring(L, strpos(L, "<-")+2, strpos(L, 'a')-1);
+		put_line(CreateChaine("la condition du pour"));
+		put_line(bi);
+		bs := substring(L, strpos(L, 'a')+1, length(L)-6);
 		bi := trimLeft(bi); bs := trimLeft(bs);
 		bi := trimRight(bi); bs := trimRight(bs);
-		put_line(bi);
-		put_line(bs);
 
 		toString(bi, binf, m);
 		tostring(bs, bsup, n);	
@@ -173,21 +185,18 @@ begin
 			Ajout_queue(descr, CreateChaine("euh... vous avez mal gérer vous bornes inf et sup"));
 			ok := False AND ok;
 		end if;
+
 	end if;
-
-
-
-	
 	return ok;
 end debug_Pour;
 
-function debug_tq(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
+function debug_repeter(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
 begin
 	-- Verifier la présence du 'faire'
 	return False;
-end debug_tq;
+end debug_repeter;
 
-function debug_repeter(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
+function debug_tq(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
 	tmp : chaine := L;
 	ss_chaine : T_tab_ligne;
 	ok : boolean := True;
@@ -196,29 +205,56 @@ begin
 	-- verifier la présence d'au moins une condition
 	SplitChaine(tmp, ss_chaine);
 	enleve_enTete(ss_chaine);
---	if donne_queue(ss_chaine) /= "faire" then
---		Ajout_queue(descr, CreateChaine("Il manque le faire a la fin de la ligne"));
---		ok := False AND ok;
---	else
---		enleve_queue(ss_chaine);
---		if estVide(ss_chaine) then 
---			Ajout_queue(descr, CreateChaine("il n'y a pas de condition"));
---		end if;
---	end if;
+	if NOT(donne_queue(ss_chaine) = CreateChaine("faire")) then
+		Ajout_queue(descr, CreateChaine("Il manque le faire a la fin de la ligne"));
+		ok := False AND ok;
+	else
+		enleve_queue(ss_chaine);
+		if estVide(ss_chaine) then 
+			Ajout_queue(descr, CreateChaine("il n'y a pas de condition"));
+			ok := False AND ok;
+		end if;
+	end if;
+
 	return ok;
-end debug_repeter;
+end debug_tq;
 
 function debug_cond(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
 begin
-	return False;
+	return True;
 end debug_cond;
 
 function debug_case(L : in chaine; descr: in out T_Tab_Ligne) return boolean is
 begin
-	return False;
+	return True;
 end debug_case;
 
 
+function analyseInclusionBloc(L : in T_Tab_ligne; descr: in out T_Tab_Ligne) return boolean is
+	ok : boolean := True;
+	tmp : T_Tab_Ligne := L;
+	pile_elt : Pile;
+begin
+	while NOT estVide(tmp) loop
+		type_ligne := GetType(donne_tete(tmp));
+		if Type_Ligne in cond | lignecas | pour | tq | repeter then
+			Ajout_queue(pile_elt, type_ligne);
+		end if;
+		put_line(CreateChaine("Avant suppression: "));
+		Affiche_liste(pile_elt);
+		if Type_Ligne in fsi | jqa | fpour | ftq then
+			enleve_queue(pile_elt);
+		end if;
+		put_line(CreateChaine("apres suppression: "));
+		Affiche_liste(pile_elt);
+		donne_suivant(tmp);
+	end loop;
+	if NOT estVide(pile_elt) then
+		Ajout_queue(descr, CreateChaine("il manque des fermeture de bloque"));
+		ok := False;
+	end if;
+	return ok;
+end;
 
 function estUneVariable(ch: chaine) return boolean is
 	ok : boolean := True;
