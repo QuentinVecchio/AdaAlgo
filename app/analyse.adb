62d3497b408fd1de6a9dc97f259c877ab10ef184
nicoptere <nicoptere@localhost>
2013-06-20 16:17:04 +0200

    je ne sais plus ce que j'ai modifier

--- app/analyse.adb
+++ app/analyse.adb
@@ -148,7 +148,7 @@ package body analyse is
 
                 --on rappel analyse code sur l'interieure de la boucle
                 Analyse_Code(tab, ListeInterne);
-                put_line(condition);
+
                 --on ajout en mémoire la boucle
                 ajoutPour(Res, condition, ListeInterne);
         
