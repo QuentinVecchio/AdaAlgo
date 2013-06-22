f168904e571a83a7882d7d1b44d5d8920608cf78
Quentin Vecchio <vecchioquentin@hotmail.fr>
2013-06-22 10:33:46 +0200

    Application final

--- app/appli/compilation.sh
+++ app/appli/compilation.sh
@@ -3,12 +3,15 @@ echo $lien
 lR1=$(echo $lien | cut -d / -f2)
 lR2=$(echo $lien | cut -d / -f3)
 lR3=$(echo $lien | cut -d / -f4)
-lR= echo '~/'$lR1'/'$lR2'/'$lR3'/'
+lR= echo 
 lC1=$(echo $lien | cut -d . -f1)
+#recupere le nom du fichier
 lC=$(echo $lC1 | cut -d  / -f5)
 echo $lR
 echo $lC
 cd $lR
-gnatmake $lien
+gnatmake '/'$lR1'/'$lR2'/'$lR3'/'$lC".adb"
+cd '/'$lR1'/'$lR2'/'$lR3'/'
+ls
 ./$lC > ~/Bureau/editeur-et-traducteur-dalgorithme/app/appli/resultat.txt
 rm $lC
